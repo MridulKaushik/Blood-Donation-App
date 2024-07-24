ae90d92459b076b3a6a5b40d0017a051e5557e97
Mridul <mridulkkaushik283@gmail.com>
2024-07-25 03:18:58 +0530

    "Changing Whitespaces"

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,25 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String, 
-        required: true
-    },
-    password:{
-        type: String,
-        required: true,
-    },
-    status:{
-        type: Number, default: 0
-    },
-    role: {
-        type: String, default: 'admin'
-    }
+    name: {type: String, required: true, trim: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    status: {type: Number, default: 0},
+    role: {type: String, default: 'admin'}
 },
     {
         timestamps: true
