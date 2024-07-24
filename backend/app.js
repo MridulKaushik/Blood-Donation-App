5e5541501b6a85bbadbf0a9a9c5cf9876fe79832
Mridul <mridulkkaushik283@gmail.com>
2024-07-25 03:15:16 +0530

    "Updates"

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+// Cors
+app.use(cors());
+
+// JSON
+app.use(express.json());
 
 app.get('', (req, res, next) => {
-
     res.send("App is working");
 });
 
 
 module.exports = app;
-
-// mridulkaushik
-// s7JdBlgo8q3dCOLH 
