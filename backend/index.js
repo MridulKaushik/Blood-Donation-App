6674a61ec72e92eec91cb50af59c238727fd9769
Mridul <mridulkkaushik283@gmail.com>
2024-07-27 17:14:33 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-console.log(process.env.PORT);
 
 app.listen(PORT, () => {
   console.log(`Go to http://localhost:${PORT}`);
