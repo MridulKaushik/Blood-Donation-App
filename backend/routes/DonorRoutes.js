dca28394a5ea8946482ac03eea50249937b7b881
Mridul <mridulkkaushik283@gmail.com>
2024-07-29 01:34:51 +0530

    Added routes

--- backend/routes/DonorRoutes.js
+++ backend/routes/DonorRoutes.js
@@ -1,4 +1,30 @@
-const { registerDonor, getAllDonors, updateDonor, getDonor, deleteDonor, getDonorStats } = require('../controllers/Donors');
-const express = require('express');
-const router = express.router();
+const {
+  registerDonor,
+  getAllDonors,
+  updateDonor,
+  getDonor,
+  deleteDonor,
+  getDonorStats,
+} = require("../controllers/Donors");
+const express = require("express");
+const router = express.Router();
 
+// Register the donor
+router.post("/register", registerDonor);
+
+// List all donors
+router.get("/", getAllDonors);
+
+// Get a donor
+router.get("/:id", getDonor);
+
+// Update Donor
+router.put("/update/:id", updateDonor);
+
+// Delete a donor
+router.delete("/delete/:id", deleteDonor);
+
+// Get Donor Stats
+router.get("/stats", getAllDonors);
+
+module.exports = router;
