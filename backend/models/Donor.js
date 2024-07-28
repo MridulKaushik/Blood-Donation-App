ce1ddf62efa75e88c265dffe5d139c7984257bb8
Mridul <mridulkkaushik283@gmail.com>
2024-07-29 01:34:22 +0530

    Added new fields & updated schema types and added default values for some fields

--- backend/models/Donor.js
+++ backend/models/Donor.js
@@ -1,18 +1,30 @@
 const mongoose = require("mongoose");
 
-const DonorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  age: { type: Number, required: true },
-  address: { type: String },
-  bloodGroup: { type: String, required: true },
-  bloodPressure: { type: Number },
-  phoneNumber: { type: String, required: true },
-  diseases: { type: [String] },
-  height: { type: Number },
-  weight: { type: Number },
-  date: { type: Date },
-  status: { type: Number, default: 0 },
-});
+const defaultDob = function () {
+  let date = new Date();
+  date.setFullYear(date.getFullYear() - 18);
+  return date;
+};
+
+const DonorSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    age: { type: Number, required: true },
+    address: { type: String, default: " " },
+    bloodGroup: { type: String, required: true },
+    bloodPressure: { type: String, default: "90-120" },
+    phoneNumber: { type: String, required: true },
+    diseases: { type: [String] },
+    height: { type: Number },
+    weight: { type: Number },
+    date: { type: Date, default: Date.now() },
+    dob: { type: Date, default: defaultDob },
+    status: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Donor", DonorSchema);
