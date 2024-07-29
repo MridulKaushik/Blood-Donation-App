0d1e59ebf36051a3a82ba28c68a0fc7d619a88d2
Mridul <mridulkkaushik283@gmail.com>
2024-07-29 15:46:02 +0530

    Added model info

--- backend/models/Prospect.js
+++ backend/models/Prospect.js
@@ -1,17 +1,24 @@
 const mongoose = require("mongoose");
 
+const calculateDob = function () {
+  let date = new Date();
+  date.setFullYear(date.getFullYear() - this.age);
+  return date;
+};
+
 const ProspectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   age: { type: Number, required: true },
-  address: { type: String },
+  address: { type: String, default: " " },
   bloodGroup: { type: String, required: true },
-  bloodPressure: { type: Number },
-  phoneNumber: { type: String },
+  bloodPressure: { type: String, default: "90-120" },
+  phoneNumber: { type: String, required: true },
   diseases: { type: [String] },
   height: { type: Number },
   weight: { type: Number },
-  date: { type: String },
+  date: { type: Date, default: Date.now() },
+  dob: { type: Date, default: calculateDob },
   status: { type: Number, default: 0 },
 });
 
